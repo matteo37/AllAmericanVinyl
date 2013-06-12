174ecfb986cb527e302198530c5b56e3f6e9f372
Bryson Thornock <thor@odin.(none)>
2013-06-11 19:49:31 -0600

    It now has mezzanine

--- mainSite/admin.py
+++
@@ -1,7 +0,0 @@
-from mainSite.models import *
-from django.contrib import admin
-
-admin.site.register(Product)
-admin.site.register(PageContent)
-admin.site.register(ContactInfo)
-admin.site.register(SideBarContent)
--- mainSite/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import RequestContext, loader, RequestContext
-def index(request):
-    return render(request,"mainSite/home.html")
