23c77eb54ecedb92ec9c8bc801beda4f52647409
Bryson Thornock <thor@odin.(none)>
2013-06-15 17:19:51 -0600

    It sort a works

--- vinylSite/views.py
+++ vinylSite/views.py
@@ -1,1 +1,15 @@
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request,"index.html")
+
+def about_us(request):
+	return render(request,"about.html")
+
+def products(request):
+	return render(request,"products.html")
+
+def contact(request):
+	return render(request,"contact.html")
 # Create your views here.
